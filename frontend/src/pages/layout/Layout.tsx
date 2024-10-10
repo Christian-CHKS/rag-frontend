24c9653e6a09ae3697715ddfeda5163104b2202e
Christian <chks@aevengroup.com>
2024-10-10 10:00:40 +0200

    updated name and picture

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -8,12 +8,12 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <img height="80px" src="https://news.microsoft.com/wp-content/uploads/prod/sites/113/2017/06/Microsoft-logo_rgb_c-gray.png"></img>
+                        <img height="80px" src="https://aevengroup.jobs.cz/assets/components/share-icons/images/logo-share-1200x630.png?av=7507700ea655e5f7"></img>
                         <h3 className={styles.headerTitle}></h3>
                     </Link>
                     <nav>
                     </nav>
-                    <h4 className={styles.headerRightText}>Chat On Your Data</h4>
+                    <h4 className={styles.headerRightText}>Seller support</h4>
                 </div>
             </header>
 
